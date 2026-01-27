261f2f9ee7a9aa51ac0f9642bc5b0fa5c299f604
Dp99088 <135794301+Dp99088@users.noreply.github.com>
2026-01-27 21:37:27 +0530

    DAY_10 CLASS

---
+++ Day_10/test_TC001.py
@@ -0,0 +1,9 @@
+import pytest
+def test_add():
+    assert 2+3 == 5
+
+def test_sub():
+    assert 10-5 == 5
+
+def test_mul():
+    assert 2*3 == 6
---
+++ Day_10/test_TC004_pms.py
@@ -0,0 +1,16 @@
+import pytest
+
+@pytest.mark.parametrize("a, b, res", [
+    (1, 2, 3),
+    (3, 4, 7)
+])
+def test_add(a, b, res):
+    assert a + b == res
+
+@pytest.mark.smoke
+def test_smoke():
+    assert True
+
+@pytest.mark.skip(reason="Not Ready")
+def test_skip():
+    pass
