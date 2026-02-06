51f5339f032a3e0202ad2a07c30861665b8cc5cd
Dp99088 <135794301+Dp99088@users.noreply.github.com>
2026-02-06 09:31:59 +0530

    DAY 17 CLASS

---
+++ DAY_17/TC004_Alerts.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Edge()
+driver.get("https://letcode.in/alert")
+driver.find_element(By.ID,"accept").click()
+alert=driver.switch_to.alert
+print(alert.text)
+alert.accept()
+
+driver.find_element(By.ID,"confirm").click()
+alert=driver.switch_to.alert
+print(alert.text)
+alert.accept()
+
+driver.find_element(By.ID,"prompt").click()
+
+alert=driver.switch_to.alert
+alert.send_keys("Durga Prasad")
+print(alert.text)
+alert.accept()
+print(driver.find_element(By.ID,"myName").text)
+
+
+driver.quit()
