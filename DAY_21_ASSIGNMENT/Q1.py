e1086d857f8cd0a2e7edd050585e60815c110862
Dp99088 <135794301+Dp99088@users.noreply.github.com>
2026-02-11 20:08:34 +0530

    DAY 21 ASSIGNMENT

---
+++ DAY_21_ASSIGNMENT/Q1.py
@@ -0,0 +1,23 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+sales = [25000, 27000, 30000, 28000, 32000, 31000]
+
+plt.figure(figsize=(8,5))
+plt.plot(months,sales,marker='o',linestyle='--')
+
+plt.title("Monthly Sales Line Chart")
+plt.xlabel("Months")
+plt.ylabel("Sales")
+plt.grid(True)
+plt.show()
+
+plt.figure(figsize=(8,5))
+sns.barplot(x=months, y=sales)
+
+plt.title("Monthly Sales Bar Chart")
+plt.xlabel("Months")
+plt.ylabel("Sales")
+plt.grid(True)
+plt.show()
